dcfc37cbce6f13af22cd0028b28b89ca1a6446f4
weiDong ZHang <doter1995@qq.com>
2019-12-28 01:54:35 +0800

    add loading page

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="loading">
-    <span class="text">加载中{{ dash }}</span>
+    <div class="loading-container" />
+    <span class="loading-text">加载中{{ dash }}</span>
   </div>
 </template>
 <script lang="ts">
@@ -11,13 +12,9 @@ export default {
     interval: null
   }),
   mounted() {
-    this.interval = setInterval(() => {
-      if (this.dash.length > 3) {
-        this.dash = "";
-      } else {
-        this.dash += ".";
-      }
-    }, 400);
+    setInterval(() => {
+      this.dash = this.dash.length > 3 ? "" : this.dash + ".";
+    }, 700);
   }
 };
 </script>
@@ -31,8 +28,60 @@ export default {
   display: flex;
   align-items: center;
   justify-content: center;
-  .text {
+  .loading-container {
+    border: 4px #333 solid;
+    border-top: 4px #b8f11a solid;
+    border-radius: 50%;
+    animation: rotate 2s infinite reverse linear;
+    width: 200px;
+    height: 200px;
+  }
+  .loading-text {
+    position: absolute;
     font-size: 1.3rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 1.3rem;
+    width: 200px;
+    height: 200px;
+    background-color: #00bcd4;
+    border-radius: 50%;
+    animation: myfirst 2s infinite reverse linear;
+  }
+  @keyframes myfirst {
+    0% {
+      background-color: rgb(65, 95, 175);
+    }
+    25% {
+      background-color: rgb(180, 75, 190);
+    }
+    50% {
+      background-color: rgb(200, 70, 190);
+    }
+    75% {
+      background-color: rgb(80, 90, 180);
+    }
+    100% {
+      background-color: rgb(65, 95, 175);
+    }
+  }
+  @keyframes rotate {
+    100% {
+      transform: rotate(0deg);
+    }
+    75% {
+      transform: rotate(90deg);
+    }
+    50% {
+      transform: rotate(180deg);
+    }
+    25% {
+      transform: rotate(240deg);
+    }
+    0% {
+      transform: rotate(360deg);
+    }
   }
 }
 </style>
--- src/layout/App.vue
+++ src/layout/App.vue
@@ -33,7 +33,7 @@ export default {
   mounted() {
     setTimeout(() => {
       this.loading = false;
-    }, 2000);
+    }, 10000);
   }
 };
 </script>
